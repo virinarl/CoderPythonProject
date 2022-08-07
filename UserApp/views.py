17dbd81356ff2b7289f839bd72ec9837e6e06762
virinarl <v.irina.rod.lap@gmail.com>
2022-08-06 22:32:15 -0300

    clean code

--- UserApp/views.py
+++ UserApp/views.py
@@ -9,15 +9,13 @@ def store(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete= False)
         cartItems = order.cart_items
-        userName = customer.name
         
     else:
         order = {'cart_items':0}
         cartItems = order['cart_items']
-        userName = ''
     
     products = Product.objects.all()
-    context={'products':products, 'cartItems': cartItems, 'userName' :userName}
+    context={'products':products, 'cartItems': cartItems}
     return render(request, 'UserApp/index.html',context)
 
 def cart(request):
@@ -26,14 +24,12 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer = customer, complete= False)
         items = order.orderitem_set.all()
         cartItems = order.cart_items
-        userName = customer.name
     else:
         items = []
         order = {'cart_total':0, 'cart_items':0}
         cartItems = order['cart_items']
-        userName = ''
     
-    context = {'items':items, 'order':order,'cartItems': cartItems, 'userName' :userName}
+    context = {'items':items, 'order':order,'cartItems': cartItems}
     return render(request, 'UserApp/cart.html',context)
 
 def checkout(request):
@@ -42,14 +38,12 @@ def checkout(request):
         order, created = Order.objects.get_or_create(customer = customer, complete= False)
         items = order.orderitem_set.all()
         cartItems = order.cart_items
-        userName = customer.name
     else:
         items = []
         order = {'cart_total':0, 'cart_items':0}
         cartItems = order['cart_items']
-        userName = ''
     
-    context = {'items':items, 'order':order, 'cartItems': cartItems, 'userName' :userName}
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
     
     return render(request, 'UserApp/checkout.html',context)
 
